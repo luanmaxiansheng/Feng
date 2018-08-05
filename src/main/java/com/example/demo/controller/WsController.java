f2f8376b8278af35dfa7dc1c85cd1f5dd21701eb
luanmaxiansheng <1586740049@qq.com>
2018-08-05 23:02:07 +0800

    xiongfeng

---
+++ src/main/java/com/example/demo/controller/WsController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.ChatMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+import java.security.Principal;
+
+/**
+ * Created by sang on 16-12-22.
+ */
+@Controller
+public class WsController {
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/chat")
+    public void handleChat(Principal principal, String msg) {
+        if (principal.getName().equals("xiong")) {
+            messagingTemplate.convertAndSendToUser("feng", "/queue/notifications", principal.getName() + "给您发来了消息：" + msg);
+        }else{
+            messagingTemplate.convertAndSendToUser("xiong", "/queue/notifications", principal.getName() + "给您发来了消息：" + msg);
+        }
+    }
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+}
